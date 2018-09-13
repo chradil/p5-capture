532c41e28e2c34cb4295d4b449f3148e92d15d0e
chradil <c_hradil@cox.net>
2018-09-13 16:20:06 -0400

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,12 @@
+var capture;
+
+function setup() {
+  createCanvas(480, 480);
+  capture = createCapture(VIDEO);
+  capture.hide();
+}
+
+function draw() {
+  image(capture, 0, 0, width, width * capture.height / capture.width);
+  filter(INVERT);
+}
