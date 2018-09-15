fb592f77838db1095f2ad84591521af6563341d2
chradil <c_hradil@cox.net>
2018-09-15 15:32:45 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -37,7 +37,7 @@ function modelReady() {
 
 function draw() {
   image(video, 0, 0, width, height);
-  image(photo, 0, 0, width, height);
+ // image(photo, 0, 0, width, height);
 
   // We can call both functions to draw all keypoints and the skeletons
   drawKeypoints();
